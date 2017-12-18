9feff0926ffa4f0254eb10c021fd58b204c3d767
William Emfinger <waemfinger@gmail.com>
2017-12-18 14:05:27 -0600

    added characteristic descriptors.

--- ns-pushtracker-test/app/settings/settings-page.js
+++ ns-pushtracker-test/app/settings/settings-page.js
@@ -34,35 +34,60 @@ function onNavigatingTo(args) {
     }
 
     try {
+        const d1 = bluetooth._bluetooth.makeDescriptor({
+            UUID: "2900",
+            permissions: android.bluetooth.BluetoothGattDescriptor.PERMISSION_READ | android.bluetooth.BluetoothGattDescriptor.PERMISSION_WRITE
+        });
+        const d2 = bluetooth._bluetooth.makeDescriptor({
+            UUID: "2902",
+            permissions: android.bluetooth.BluetoothGattDescriptor.PERMISSION_READ | android.bluetooth.BluetoothGattDescriptor.PERMISSION_WRITE
+        });
+
         const data_control = bluetooth._bluetooth.makeAdvCharacteristic({
             UUID: "58daaa15-f2b2-4cd9-b827-5807b267dae1",
             gattProperty: android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ | android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE | android.bluetooth.BluetoothGattCharacteristic.PROPERTY_INDICATE,
             gattPermissions: android.bluetooth.BluetoothGattCharacteristic.PERMISSION_WRITE | android.bluetooth.BluetoothGattCharacteristic.PERMISSION_READ
         });
+        data_control.addDescriptor(d1);
+        data_control.addDescriptor(d2);
+
         const App_data = bluetooth._bluetooth.makeAdvCharacteristic({
             UUID: "68208ebf-f655-4a2d-98f4-20d7d860c471",
             gattProperty: android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ | android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE | android.bluetooth.BluetoothGattCharacteristic.PROPERTY_INDICATE,
             gattPermissions: android.bluetooth.BluetoothGattCharacteristic.PERMISSION_WRITE | android.bluetooth.BluetoothGattCharacteristic.PERMISSION_READ
         });
+        App_data.addDescriptor(d1);
+        App_data.addDescriptor(d2);
+
         const OTA_data = bluetooth._bluetooth.makeAdvCharacteristic({
             UUID: "9272e309-cd33-4d83-a959-b54cc7a54d1f",
             gattProperty: android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ | android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE | android.bluetooth.BluetoothGattCharacteristic.PROPERTY_INDICATE,
             gattPermissions: android.bluetooth.BluetoothGattCharacteristic.PERMISSION_WRITE | android.bluetooth.BluetoothGattCharacteristic.PERMISSION_READ
         });
+        OTA_data.addDescriptor(d1);
+        OTA_data.addDescriptor(d2);
+
         const WB_data = bluetooth._bluetooth.makeAdvCharacteristic({
             UUID: "8489625f-6c73-4fc0-8bcc-735bb173a920",
             gattProperty: android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ | android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE | android.bluetooth.BluetoothGattCharacteristic.PROPERTY_INDICATE,
             gattPermissions: android.bluetooth.BluetoothGattCharacteristic.PERMISSION_WRITE | android.bluetooth.BluetoothGattCharacteristic.PERMISSION_READ
         });
+        WB_data.addDescriptor(d1);
+        WB_data.addDescriptor(d2);
+
         const DU_data = bluetooth._bluetooth.makeAdvCharacteristic({
             UUID: "5177fda8-1003-4254-aeb9-7f9edb3cc9cf",
             gattProperty: android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ | android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE | android.bluetooth.BluetoothGattCharacteristic.PROPERTY_INDICATE,
             gattPermissions: android.bluetooth.BluetoothGattCharacteristic.PERMISSION_WRITE | android.bluetooth.BluetoothGattCharacteristic.PERMISSION_READ
         });
+        DU_data.addDescriptor(d1);
+        DU_data.addDescriptor(d2);
+
         const app_service = bluetooth._bluetooth.makeAdvService({
             UUID: "9358ac8f-6343-4a31-b4e0-4b13a2b45d86",
             serviceType: android.bluetooth.BluetoothGattService.SERVICE_TYPE_PRIMARY
         });
+
         app_service.addCharacteristic(data_control);
         app_service.addCharacteristic(App_data);
         app_service.addCharacteristic(OTA_data);
