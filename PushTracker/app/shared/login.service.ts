25d450feba7b2dbe693ccaaa40efdb4fd491c109
William Emfinger <waemfinger@gmail.com>
2018-01-11 10:52:04 -0600

    minor update.

--- PushTracker/app/shared/login.service.ts
+++ PushTracker/app/shared/login.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from "@angular/http";
-import { Observable } from "rxjs/Rx";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/catch";
 import "rxjs/add/operator/do";
 import "rxjs/add/operator/map";
 
